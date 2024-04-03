5a33796d4a8e8433a58d45ce3b0ffa4908347864
Shashank Devadiga <beast.steam002@gmail.com>
2024-04-03 19:59:35 +0530

    added createcard function

--- Project-03 [Spotify Clone]/script.js
+++ Project-03 [Spotify Clone]/script.js
@@ -1,31 +1,21 @@
-console.log(`js connected`)
-
-const card = 
-`
-<div class="card-structure">
-    <img class="playlist-img" src="assets/BEAST GAMING (2).png" alt="">
-    <h3 class="p-title">Beast Music! </h3>
-    <p class="p-desc"> Lorem ipsum dolor sit amet consectetur adipisicing elit.  </p>
-    <img  class="play"  src="assets/play.svg" alt="play">
-</div>
-`;
-
-
-
-function cards () {
-    let playlists = document.querySelector(".playlist-card")
-    playlists.innerHTML = playlists.innerHTML + card;
+function createCard(title, desc, imageSrc) {
+    return `
+    <div class="card-structure">
+        <img class="playlist-img" src="${imageSrc}" alt="image">
+        <h3 class="p-title">${title}</h3>
+        <p class="p-desc">${desc}</p>
+        <img class="play" src="assets/play.svg" alt="play">
+    </div>
+    `;
 }
 
-cards()
-cards()
-cards()
-cards()
-cards()
-cards()
-cards()
-cards()
-cards()
-cards()
-cards()
-cards()
+function addCardToPlaylist(title, desc, imageSrc) {
+    let playlists = document.querySelector(".playlist-card");
+    playlists.innerHTML += createCard(title, desc, imageSrc);
+}
+
+
+addCardToPlaylist("Test", "This is a example description.This is to increase the line ", "https://png.pngtree.com/png-vector/20190408/ourmid/pngtree-vector-music-illustration-with-wing-and-speaker-png-image_923833.jpg")
+addCardToPlaylist("Test", "This is a example description. ", "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/sphere-glass-the-mixtape-cd-cover-design-template-73ab5b3d9b81f442cb2288630ab63acf.jpg?ts=1602178819")
+addCardToPlaylist("Test", "This is a example description. ", "https://img.freepik.com/free-vector/realistic-music-record-label-disk-mockup_1017-33906.jpg")
+addCardToPlaylist("Test", "This is a example description. ", "https://img.freepik.com/free-vector/realistic-music-record-label-disk-mockup_1017-33906.jpg")
