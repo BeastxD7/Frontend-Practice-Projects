0e3b51ab0c7afa13d22ee4b801d3edba742d48b3
Shashank Devadiga <beast.steam002@gmail.com>
2024-04-06 14:09:24 +0530

    Added RealTime SongList in Your Library Section

--- Project-03 [Spotify Clone]/script.js
+++ Project-03 [Spotify Clone]/script.js
@@ -1,3 +1,5 @@
+
+
 function createCard(title, desc, imageSrc) {
     return `
     <div class="card-structure">
@@ -13,6 +15,33 @@ function addCardToPlaylist(title, desc, imageSrc) {
     let playlists = document.querySelector(".playlist-card");
     playlists.innerHTML += createCard(title, desc, imageSrc);
 }
+
+
+
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:5500/songs/"); 
+    let response = await a.text(); 
+    console.log(response); 
+
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName('a')
+    console.log(as)
+
+    for (let i = 3; i < as.length; i++) {
+        let songtitle = as[i].innerText;
+        console.log(songtitle)
+        songtitle = songtitle.split(".mp3")[0]
+        let songs = document.querySelector("#songs");
+        songs.innerHTML += ` <div  class="card" style="list-style: none;">
+        <li > ${songtitle}</li>
+        </div>`    
+        
+    }
+}
+getSongs()
+
+// let songtitle = "srinivas";
 
 
 addCardToPlaylist("Test", "This is a example description.This is to increase the line ", "https://png.pngtree.com/png-vector/20190408/ourmid/pngtree-vector-music-illustration-with-wing-and-speaker-png-image_923833.jpg")
