45384292febdc6129406621ef073197636b47d0b
BeastxD7 <shashankdevadiga2003@gmail.com>
2025-03-08 23:51:02 +0530

    update

---
+++ useContext/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-zinc-900 text-white w-screen h-screen shadow-md flex justify-center items-center"> 
+      hi there!
+    </div>
+  )
+}
+
+export default App
