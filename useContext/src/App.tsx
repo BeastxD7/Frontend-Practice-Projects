f6f142b7ab2fa33ac4df46cf55b4e66af47b8aea
BeastxD7 <shashankdevadiga2003@gmail.com>
2025-03-11 16:50:24 +0530

    removed context proj

--- useContext/src/App.tsx
+++
@@ -1,9 +0,0 @@
-const App = () => {
-  return (
-    <div className="bg-zinc-900 text-white w-screen h-screen shadow-md flex justify-center items-center"> 
-      hi there!
-    </div>
-  )
-}
-
-export default App
